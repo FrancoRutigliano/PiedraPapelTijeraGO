07c7f9eb6afdb89c2b42fa499dffd0d13ed95c16
FrancoRutigliano <francorutigliano11@gmail.com>
2023-12-24 19:38:06 -0300

    Creando estructura de resultados para cada ronda

---
+++ ppt/ppt.go
@@ -0,0 +1,18 @@
+package ppt
+
+// Lógica de juego
+
+const (
+	PIEDRA = 0
+	PAPEL  = 1
+	TIJERA = 2
+)
+
+type Round struct {
+	Message           string `json:"message"`
+	ComputerChoice    string `json:"computer_choice"`
+	RoundResult       string `json:"round_result"`
+	ComputerChoiceInt int    `json:"computer_choice_int"`
+	ComputerScore     string `json:"computer_score"`
+	PlayerScore       string `json:"player_score"`
+}
